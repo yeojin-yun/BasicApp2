075bc5f6401d62f6b6da437dcf4a7c8d13afa400
yeojinyun <sail041@naver.com>
2022-03-22 21:49:45 +0900

    Update CalculateLogic

--- 3. Calculate/Calculate/CalculatorLogic.swift
+++ 3. Calculate/Calculate/CalculatorLogic.swift
@@ -7,24 +7,52 @@
 
 import UIKit
 
-class CalculateLogic {
+struct CalculateLogic {
     
-    let number: Double
+    private var number: Double?
     
-    init(number: Double) {
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
+    mutating func setNumber(_ number: Double) {
         self.number = number
     }
     
-    func calculate(symbol: String) -> Double? {
-        switch symbol {
-        case "+/-":
-            return number * -1
-        case "AC":
-            return 0
-        case "%":
-            return number * 0.01
-        default:
-            return nil
+    mutating func calculate(symbol: String) -> Double? {
+        if let n = number {
+            
+            if symbol == "+/-" {
+                return n * -1
+            } else if symbol == "AC" {
+                return 0
+            } else if symbol == "%" {
+                return n * 0.01
+            } else if symbol == "=" {
+                return performTwoNumCalculate(n2: n)
+            } else {
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
         }
+        return nil
+    }
+    
+    private func performTwoNumCalculate(n2: Double) -> Double? {
+        //튜플을 옵셔널로 설정했기 때문에 옵셔널 바인딩 필요
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "x":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("The operation passed in does not match any of the cases")
+            }
+        }
+        return nil
     }
 }
+
+
--- 3. Calculate/Calculate/ViewController.swift
+++ 3. Calculate/Calculate/ViewController.swift
@@ -9,12 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//    var preStringArray: [String] = []
-//    var preStringNumber: String = "0"
-//    var preStringToInt: Int = 0
-    
     private var isFinishedTypingNumber = true
     //private : 다른 클래스에서 접근 불가능 (해당 스코프에서만 접근 가능)
+    
+    private var calculator = CalculateLogic()
     
     private var displayValue: Double {
         get {
@@ -60,40 +58,45 @@ class ViewController: UIViewController {
 
 //MARK: -Button Event
 extension ViewController {
-    //숫자 버튼을 누르면 숫자가 previousArray로 들어가고 그걸 다시 stringNumber값으로 보냄
+    // 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, . 버튼에 관한 이벤트
     @objc private func numberPadTapped(_ sender: UIButton) {
         
-        //현재 버튼의 이름이 resultLbl에 나타나도록
-        if let numValue = sender.currentTitle {
-            if isFinishedTypingNumber {
-                resultLbl.text = numValue
-                isFinishedTypingNumber = false
-            } else {
-                if numValue == "." {
-                    let isInt = floor(displayValue) == displayValue
+        //현재 버튼의 text가 resultLbl에 나타나도록
+        if let numValue = sender.currentTitle { //sender.currentTitle가 옵셔널 값이라 바인딩 필요
+            if isFinishedTypingNumber { //isFinishedTypingNumber의 기본값이 true
+                resultLbl.text = numValue //결과 레이블에 누른 값을 표시하고
+                isFinishedTypingNumber = false //isFinishedTypingNumber의 값을 다시 false로 설정 => 이제부터는 else 구문이 실행됨
+                print("if 구문 출력, \(isFinishedTypingNumber)")
+            } else { //위에서 false로 설정했기 때문에 이제 아래 구문만 계속 실행됨
+                print("else 구문 출력, \(isFinishedTypingNumber)")
+                if numValue == "." { //.가 1번 이상 눌리지 못하도록 설정
                     
-                    if floor(displayValue) != displayValue {
-                        return
+                    if floor(displayValue) != displayValue { // 누른 값(displayValue)의 소수점을 버린 값 != 누른 값이면
+                        print("floor함수 = \(floor(displayValue))")
+                        print("\(displayValue)")
+                        return // floor 함수가 종료되고, 레이블에 추가하는 함수가 작동
                     }
                     
                 }
-                
                 // 누른 버튼의 타이틀이 Label에 누적되도록
                 resultLbl.text = resultLbl.text! + numValue
+                print("추가추가")
             }
         }
         
         
         
     }
+    //"AC", "+/-", "%", "+", "-", "x", "÷"
     @objc private func calculateBtnTapped(_ sender: UIButton) {
         isFinishedTypingNumber = true
 
-        
+        calculator.setNumber(displayValue)
         if let calcalutaMethod = sender.currentTitle {
-            let calculator = CalculateLogic(number: displayValue)
-            guard let result = calculator.calculate(symbol: calcalutaMethod) else { fatalError("The result of the calculation is nil.")}
-             displayValue = result
+            print(calcalutaMethod)
+            if let result = calculator.calculate(symbol: calcalutaMethod) { displayValue = result //set구문 실행 -> resultLbl.text = String(newValue)
+            }
+             
         }
     }
     
@@ -140,21 +143,19 @@ extension ViewController {
     }
     
     final private func addTarget() {
-        [allClearBtn, plusMinusBtn, percentageBtn, decimalPointBtn].forEach {
+        [allClearBtn, plusMinusBtn, percentageBtn].forEach {
             $0.addTarget(self, action: #selector(calculateBtnTapped(_:)), for: .touchUpInside)
         }
 
-        [oneBtn, twoBtn, threeBtn, fourBtn, fiveBtn, sixBtn, sevenBtn, eightBtn, nineBtn, zeroBtn].forEach { 
+        [oneBtn, twoBtn, threeBtn, fourBtn, fiveBtn, sixBtn, sevenBtn, eightBtn, nineBtn, zeroBtn, decimalPointBtn].forEach {
             $0.addTarget(self, action: #selector(numberPadTapped(_:)), for: .touchUpInside)
         }
 
-        
-
-        divideBtn.addTarget(self, action: #selector(divideBtnTapped(_:)), for: .touchUpInside)
-        multiplyBtn.addTarget(self, action: #selector(multiplyBtnTapped(_:)), for: .touchUpInside)
-        subtractBtn.addTarget(self, action: #selector(subtractBtnTapped(_:)), for: .touchUpInside)
+        divideBtn.addTarget(self, action: #selector(calculateBtnTapped(_:)), for: .touchUpInside)
+        multiplyBtn.addTarget(self, action: #selector(calculateBtnTapped(_:)), for: .touchUpInside)
+        subtractBtn.addTarget(self, action: #selector(calculateBtnTapped(_:)), for: .touchUpInside)
         addBtn.addTarget(self, action: #selector(calculateBtnTapped(_:)), for: .touchUpInside)
-        resultBtn.addTarget(self, action: #selector(resultBtnTapped(_:)), for: .touchUpInside)
+        resultBtn.addTarget(self, action: #selector(calculateBtnTapped(_:)), for: .touchUpInside)
     }
     
     final private func setConstraints() {
@@ -227,92 +228,3 @@ extension ViewController {
         ])
     }
 }
-
-//
-//    @objc private func oneTapped(_ sender: UIButton) {
-//        if let currentInt = sender.currentTitle {
-//            previousArray.append(currentInt)
-//        }
-//        let newString = previousArray.joined()
-//        print(newString)
-//        resultLbl.text = previousArray.joined()
-//    }
-//
-//    @objc private func twoTapped(_ sender: UIButton) {
-//        if let currentInt = sender.currentTitle {
-//            previousArray.append(currentInt)
-//        }
-//        let newString = previousArray.joined()
-//        print(newString)
-//        resultLbl.text = previousArray.joined()
-//    }
-//
-//    @objc private func threeTapped(_ sender: UIButton) {
-//        if let currentInt = sender.currentTitle {
-//            previousArray.append(currentInt)
-//        }
-//        let newString = previousArray.joined()
-//        print(newString)
-//        resultLbl.text = previousArray.joined()
-//    }
-//
-//    @objc private func fourTapped(_ sender: UIButton) {
-//        if let currentInt = sender.currentTitle {
-//            previousArray.append(currentInt)
-//        }
-//        let newString = previousArray.joined()
-//        print(newString)
-//        resultLbl.text = previousArray.joined()
-//    }
-//
-//    @objc private func fiveTapped(_ sender: UIButton) {
-//        if let currentInt = sender.currentTitle {
-//            previousArray.append(currentInt)
-//        }
-//        let newString = previousArray.joined()
-//        print(newString)
-//        resultLbl.text = previousArray.joined()
-//    }
-//
-//    @objc private func sixTapped(_ sender: UIButton) {
-//        if let currentInt = sender.currentTitle {
-//            previousArray.append(currentInt)
-//        }
-//        let newString = previousArray.joined()
-//        print(newString)
-//        resultLbl.text = previousArray.joined()
-//    }
-//
-//    @objc private func sevenTapped(_ sender: UIButton) {
-//        if let currentInt = sender.currentTitle {
-//            previousArray.append(currentInt)
-//        }
-//        let newString = previousArray.joined()
-//        print(newString)
-//        resultLbl.text = previousArray.joined()
-//    }
-//
-//    @objc private func eightTapped(_ sender: UIButton) {
-//        if let currentInt = sender.currentTitle {
-//            previousArray.append(currentInt)
-//        }
-//        let newString = previousArray.joined()
-//        print(newString)
-//        resultLbl.text = previousArray.joined()
-//    }
-//    @objc private func nineTapped(_ sender: UIButton) {
-//        if let currentInt = sender.currentTitle {
-//            previousArray.append(currentInt)
-//        }
-//        let newString = previousArray.joined()
-//        print(newString)
-//        resultLbl.text = previousArray.joined()
-//    }
-//    @objc private func zeroBtnTapped(_ sender: UIButton) {
-//        if let currentInt = sender.currentTitle {
-//            previousArray.append(currentInt)
-//        }
-//        let newString = previousArray.joined()
-//        print(newString)
-//        resultLbl.text = previousArray.joined()
-//    }
