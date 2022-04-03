5088d47b2c43f05ef27251385bcc93fd70ab19a5
yeojinyun <sail041@naver.com>
2022-04-03 22:20:09 +0900

    Add New Project

---
+++ 5. The Artist/5. The Artist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5. The Artist
+//
+//  Created by 순진이 on 2022/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
